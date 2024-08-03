2b1ce8561f1d10dae50feb98e0d155f81eb2f476
D0G33 <brysonrogersmith@gmail.com>
2024-08-02 20:41:46 -0500

    Second Push

---
+++ 7zManager.py
@@ -0,0 +1,10 @@
+import os
+
+def zip(file,name="package"):
+    os.system("del "+name+".7z")
+    os.system("tools\\7zr.exe a "+name+".7z "+file+"")
+
+def unzip(file):
+    os.system("mkdir Extracted")
+    os.system('tools\\7zr.exe e '+file)
+
